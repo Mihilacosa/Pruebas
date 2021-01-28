f1da621f3f0a63e30249b02b7fb8bfa67b2bccc2
Mihilacosa <mhosliag@educantabria.es>
2021-01-28 19:22:50 +0100

    prueba boton

--- app/src/main/java/com/example/pruebas/ProfileFragment.java
+++ app/src/main/java/com/example/pruebas/ProfileFragment.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    Button boton;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        boton = (Button) getView().findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Send", Toast.LENGTH_SHORT).show();
+            }
+        });
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
     }
 }
